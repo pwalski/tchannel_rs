11a765687752585faf3f2c984825f42d78c8a501
Przemysław Walski <pwalski@users.noreply.github.com>
2020-12-28 22:27:19 +0100

    Project initial setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tchannel-rust
+
---
+++ tchannel-core/src/lib.rs
@@ -0,0 +1,53 @@
+
+
+pub mod tchannel {
+    use std::io;
+
+    fn newChannel(name: String) -> Result<Channel, io::Error> {
+        let peers = PeerList{};
+        let consectionOptions = ConnectionOptions{};
+        let createdStack = String::from("stack");
+        let channel = Channel{id: 1, createdStack, consectionOptions, peers};
+        Ok(channel)
+    }
+
+    pub struct Channel {
+        id: u32,
+        createdStack: String,
+        consectionOptions: ConnectionOptions,
+        peers: PeerList
+    }
+
+    pub struct ConnectionOptions {
+
+    }
+
+    pub struct PeerList {
+
+    }
+
+    pub struct Handler {
+
+    }
+
+    pub enum ConnectionDirection {
+        None,
+        In,
+        Out
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn functional_test() {
+        // - new channel for client
+
+        // - register handler to channel
+
+        // - add peer using addr string
+        // - create subchannel using serviceName string
+        // - create client using channel, subchannel, clientoptions, and serviceName
+        assert_eq!(2 + 2, 4);
+    }
+}
